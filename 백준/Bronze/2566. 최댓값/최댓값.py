53b07d0f997129c898cce92e6d108244f6ab0e3e
김현진 <48575838+ki1029m@users.noreply.github.com>
2023-10-14 21:17:43 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -0,0 +1,19 @@
+def solve():
+  table = []
+  for _ in range(9):
+    table.append(list(map(int, input().split())))
+  
+  max_num = 0
+  max_row, max_col = 0, 0
+  
+  for row in range(9):
+      for col in range(9):
+          if max_num <= table[row][col]:
+              max_row = row + 1
+              max_col = col + 1
+              max_num = table[row][col]
+
+  print(max_num)
+  print(max_row, max_col)
+
+solve()
